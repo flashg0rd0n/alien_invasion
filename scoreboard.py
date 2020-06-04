0ab0e2f8844cae2ead71b3485b9714b953707914
flash <flashg0rd0n@hotmail.com>
2020-06-04 19:22:21 +0100

    alien_invasion.py:  A comment line has been deleted game_functions.py:  A new_level() function has been added to refactor code scoreboard.py:  prep_images() function added to refactore the image calls

--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,9 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
+        self.prep_images()
+
+    def prep_images(self):
         # Prepare the initial score image.
         self.prep_score()
         self.prep_high_score()
